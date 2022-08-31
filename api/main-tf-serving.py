af4c949dbc7f6ccf52b34110608581e2af90cd9a
Biswajit Saha <sahabiswajit600@gmail.com>
2022-08-31 23:26:37 +0530

    tf serving backend

--- api/main-tf-serving.py
+++ api/main-tf-serving.py
@@ -4,12 +4,12 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+import requests
 
 app = FastAPI()
 
 endpoint = "http://localhost:8501/v1/models/monkeypox_model:predict"
 
-MODEL = tf.keras.models.load_model("../models/1")
 CLASS_NAMES = ["Monkeypox", "Others"]
 
 @app.get("/ping")
@@ -27,13 +27,20 @@ async def predict(
 ):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
-    predictions = MODEL.predict(img_batch)
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence = np.max(predictions[0])
+    
+    json_data = {
+        "instances": img_batch.tolist()
+    }
+    
+    response = requests.post(endpoint, json=json_data)
+    prediction = np.array(response.json()["predictions"][0])
+
+    predicted_class = CLASS_NAMES[np.argmax(prediction)]
+    confidence = np.max(prediction)
 
     return {
-        'class': predicted_class,
-        'confidence': float(confidence)
+        "class": predicted_class,
+        "confidence": float(confidence)
     }
 
 if __name__ == "__main__":
